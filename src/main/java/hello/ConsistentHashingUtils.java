213a4e8f9903e35003b07b1d9c2d27761a42761a
Ludwik Bukowski <ludwik.bukowski@gmail.com>
2018-02-18 17:03:54 +0100

    draft of consistent hashing

---
+++ src/main/java/hello/ConsistentHashingUtils.java
@@ -0,0 +1,27 @@
+package hello;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by ludwikbukowski on 12/02/18.
+ */
+public class ConsistentHashingUtils {
+    // 2^32 as a range for keys
+    private static Integer keyDomain = java.lang.Integer.MAX_VALUE;
+
+    public static Integer getNode(String key) {
+        Integer code = abs(key.hashCode());
+        Integer range = keyDomain / Main.NODES_NUMBER;
+        System.out.println("range is " + range + " and code " + code);
+        int part = 1;
+        while(code > part * range){part++;}
+        return part;
+    }
+
+    private static Integer abs(int i) {
+        if(i<0) return -i;
+        return i;
+    }
+}
